cdeba668a4c02bc1e50a8868448c01e05119f084
farid-ss3945 <abbaslifarid2006@gmail.com>
2025-09-19 12:03:49 +0400

    Add files via upload

---
+++ Launcher/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace Launcher
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var processes = Process.GetProcessesByName("Launcher");
+            if (processes.Length > 1)
+            {
+                MessageBox.Show("Лаунчер уже запущен!");
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void Launch_Click(object sender, RoutedEventArgs e)
+        {
+            string operation = OperationBox.Text;
+            string a = Box_1.Text;
+            string b = Box_2.Text;
+            string debug = DebugCheck.IsChecked == true ? "true" : "false";
+            string logfile = LogFileBox.Text;
+
+            string consolePath = Path.GetFullPath(
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"C:\Users\MSI1\RiderProjects\WpfApp4\ConsoleApp1\bin\Debug\net9.0\ConsoleApp1.exe")
+            );
+
+            if (!File.Exists(consolePath))
+            {
+                MessageBox.Show($"ConsoleApp.exe не найден по пути:\n{consolePath}");
+                return;
+            }
+
+            Process.Start(consolePath, $"{operation} {a} {b} {debug} {logfile}");
+            Application.Current.Shutdown();
+        }
+
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Launcher/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string operation = args[0];
+        if (!double.TryParse(args[1], out double a) || !double.TryParse(args[2], out double b))
+        {
+            Log(args[4], args[3], $"Error: incorrect arguments '{args[1]}' и '{args[2]}'");
+            return;
+        }
+
+        bool debug = bool.Parse(args[3]);
+        string logFile = args[4];
+
+        try
+        {
+            double result = operation switch
+            {
+                "add" => a + b,
+                "sub" => a - b,
+                "mul" => a * b,
+                "div" => b != 0 ? a / b : throw new DivideByZeroException(),
+                _ => throw new ArgumentException("Unknown Operation")
+            };
+
+            Console.WriteLine($"Result: {result}");
+            Log(logFile, debug.ToString(), $"Operation {operation} with {a} and {b} = {result}");
+            Console.WriteLine("Press any button to quit...");
+        }
+        catch (Exception ex)
+        {
+            Log(logFile, debug.ToString(), $"Error: {ex.Message}");
+        }
+        Console.WriteLine("Press any button to quit...");
+        Thread.Sleep(50000);
+    }
+
+    static void Log(string file, string debug, string message)
+    {
+        if (bool.TryParse(debug, out bool isDebug) && isDebug)
+        {
+            string logMessage = $"[{DateTime.Now}] {message}";
+            Console.WriteLine(logMessage);
+            File.AppendAllText(file, logMessage + Environment.NewLine);
+        }
+    }
+    
+
+}
