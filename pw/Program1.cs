70dae55ed087b4e55e6c019b6a951a6efcdb2063
farid-ss3945 <abbaslifarid2006@gmail.com>
2025-09-19 11:27:40 +0400

    Add files via upload

---
+++ pw/Program1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+var oddEvent = new EventWaitHandle(true, EventResetMode.AutoReset, "OddEvent");
+var evenEvent = new EventWaitHandle(false, EventResetMode.AutoReset, "EvenEvent");
+
+for (int i = 1; i <= 10; i += 2)
+{
+    oddEvent.WaitOne(); 
+    Console.WriteLine($"Odd: {i}");
+    Thread.Sleep(500);
+    evenEvent.Set();
+}
+
+
